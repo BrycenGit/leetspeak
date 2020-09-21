594696e6b95a455d9c8c5440c7f51119596a93b0
drewparker <drewjparker91@gmail.com>
2020-09-21 15:50:12 -0700

    refactor to regex co-authored-by: Donny Mays <dtmays2@gmail.com> co-authored-by: brycen bartolome <brycenbartolome@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,3 +1,28 @@
 def leetspeak(string)
-  split_sentence = string.split("")
+  string1 = string.gsub(/[EeoOI]/, 'E' => 3, 'e' => 3, 'o' => 0, 'O' => 0, 'I' => 1)
+  string2 = string1.gsub!(/(?<=\S)[s]/i,"z")
+  string2
 end
+
+
+
+# sentence_split = sentence.split(' ')
+# sentence_split.each do |word|
+#   if (word)
+#     word = word.gsub!(/\Ss/i,"z")
+#   end
+# end 
+
+# elsif (letter)
+#   letter = letter.sub!(/s/i, "z")
+  
+  
+  #   if string1.include?("o") 
+    # string2 = string1.sub!(/o/,"0")
+# (/e,o/ "3","0")\
+
+# if (letter[i] = 0 || if letter comes after ?<whitespace)
+
+# if non-whitespace character is behind s -> z
+
+#   ?<=s
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -2,7 +2,20 @@ require ('rspec')
 require ('leetspeak.rb')
 
 describe('leetspeak') do
-  it('splits string into an array by character') do
-    expect(leetspeak("hello")).to(eq(["h","e","l","l","o"]))
+
+  it('replaces the letter "e" with "3"') do
+    expect(leetspeak("jell")).to(eq("j3ll"))
+  end
+
+  it('replaces the letter "o" with "0"') do
+    expect(leetspeak("jello")).to(eq("j3ll0"))
+  end
+
+  it('replaces all "I" with "1"') do
+    expect(leetspeak("jEllo boi Igloo")).to(eq("j3ll0 b0i 1gl00"))
+  end
+
+  it('replaces "s" with "z"') do
+    expect(leetspeak("jEllo boi sssS Igloo snOose x")).to(eq("j3ll0 b0i 1gl00 sn00z3"))
   end
 end
